6039fcab9126b17fa533d00a6c52a268e237fa45
dimatteo <>
2013-09-11 15:54:34 +0000

    added PU syst and other variables

--- Core/MitGPTreeReduced.h
+++ Core/MitGPTreeReduced.h
@@ -23,6 +23,8 @@ class MitGPTreeReduced {
   float          evt_weight_;
   float          hlt_weight_;
   float          pu_weight_;
+  float          pu_weightup_;
+  float          pu_weightdo_;
   float          kf_weight_;
 
   unsigned int   nvtx_;
@@ -31,6 +33,7 @@ class MitGPTreeReduced {
   float          sumEt_;
   float          metSig_;
 
+  int            nphotons_;
   float          phoEt_;
   float          phoEta_;
   float          phoPhi_;
@@ -38,12 +41,37 @@ class MitGPTreeReduced {
   float          phoMetDeltaPhi_;
 
   unsigned int   njets_;
-  float          leadJetEt_;
+  float          leadJetPt_;
   float          leadJetEta_;
   float          leadJetPhi_;
-  float          trailJetEt_;
+  float          trailJetPt_;
   float          trailJetEta_;
   float          trailJetPhi_;
+  
+  //for MET syst
+  unsigned int   nalljets_;
+  float  jet1Pt_;
+  float  jet1Eta_;
+  float  jet2Pt_;
+  float  jet2Eta_;
+  float  jet3Pt_;
+  float  jet3Eta_;
+  float  jet4Pt_;
+  float  jet4Eta_;
+
+  //for Lep syst
+  unsigned int   nlep_;
+  float  lep1Pt_;
+  float  lep1Eta_;
+  int    lep1Id_;
+  float  lep2Pt_;
+  float  lep2Eta_;
+  int    lep2Id_;
+  float  lep3Pt_;
+  float  lep3Eta_;
+  int    lep3Id_;
+
+  unsigned int   ncosmics_;
 
  public:
   /// this is the main element
@@ -82,6 +110,8 @@ class MitGPTreeReduced {
     tree_->Branch("evt_weight"        , &evt_weight_        ,   "evt_weight/F");
     tree_->Branch("hlt_weight"        , &hlt_weight_        ,   "hlt_weight/F");
     tree_->Branch("pu_weight"         , &pu_weight_         ,   "pu_weight/F");
+    tree_->Branch("pu_weightup"       , &pu_weightup_       ,   "pu_weightup/F");
+    tree_->Branch("pu_weightdo"       , &pu_weightdo_       ,   "pu_weightdo/F");
     tree_->Branch("kf_weight"         , &kf_weight_         ,   "kf_weight/F");
   
     tree_->Branch("nvtx"         , &nvtx_         ,   "nvtx/i");
@@ -90,6 +120,7 @@ class MitGPTreeReduced {
     tree_->Branch("sumEt"        , &sumEt_        ,   "sumEt/F");
     tree_->Branch("metSig"       , &metSig_       ,   "metSig/F");
   
+    tree_->Branch("nphotons"      , &nphotons_      ,   "nphotons/i");
     tree_->Branch("phoEt"         , &phoEt_         ,   "phoEt/F");
     tree_->Branch("phoEta"        , &phoEta_        ,   "phoEta/F");
     tree_->Branch("phoPhi"        , &phoPhi_        ,   "phoPhi/F");
@@ -97,12 +128,35 @@ class MitGPTreeReduced {
     tree_->Branch("phoMetDeltaPhi", &phoMetDeltaPhi_,   "phoMetDeltaPhi/F");
   
     tree_->Branch("njets"         , &njets_         ,   "njets/i");
-    tree_->Branch("leadJetEt"         , &leadJetEt_         ,   "leadJetEt/F");
+    tree_->Branch("leadJetPt"         , &leadJetPt_         ,   "leadJetPt/F");
     tree_->Branch("leadJetEta"        , &leadJetEta_        ,   "leadJetEta/F");
     tree_->Branch("leadJetPhi"        , &leadJetPhi_        ,   "leadJetPhi/F");
-    tree_->Branch("trailJetEt"        , &trailJetEt_        ,   "trailJetEt/F");
+    tree_->Branch("trailJetPt"        , &trailJetPt_        ,   "trailJetPt/F");
     tree_->Branch("trailJetEta"       , &trailJetEta_       ,   "trailJetEta/F");
     tree_->Branch("trailJetPhi"       , &trailJetPhi_       ,   "trailJetPhi/F");
+
+    tree_->Branch("nalljets"          , &nalljets_          ,   "nalljets/i");
+    tree_->Branch("jet1Pt"            , &jet1Pt_            ,   "jet1Pt/F");
+    tree_->Branch("jet1Eta"           , &jet1Eta_           ,   "jet1Eta/F");
+    tree_->Branch("jet2Pt"            , &jet2Pt_            ,   "jet2Pt/F");
+    tree_->Branch("jet2Eta"           , &jet2Eta_           ,   "jet2Eta/F");
+    tree_->Branch("jet3Pt"            , &jet3Pt_            ,   "jet3Pt/F");
+    tree_->Branch("jet3Eta"           , &jet3Eta_           ,   "jet3Eta/F");
+    tree_->Branch("jet4Pt"            , &jet4Pt_            ,   "jet4Pt/F");
+    tree_->Branch("jet4Eta"           , &jet4Eta_           ,   "jet4Eta/F");
+
+    tree_->Branch("nlep"              , &nlep_              ,   "nlep/i");
+    tree_->Branch("lep1Pt"            , &lep1Pt_            ,   "lep1Pt/F");
+    tree_->Branch("lep1Eta"           , &lep1Eta_           ,   "lep1Eta/F");
+    tree_->Branch("lep1Id"            , &lep1Id_            ,   "lep1Id_/i");
+    tree_->Branch("lep2Pt"            , &lep2Pt_            ,   "lep2Pt/F");
+    tree_->Branch("lep2Eta"           , &lep2Eta_           ,   "lep2Eta/F");
+    tree_->Branch("lep2Id"            , &lep2Id_            ,   "lep2Id_/i");
+    tree_->Branch("lep3Pt"            , &lep3Pt_            ,   "lep3Pt/F");
+    tree_->Branch("lep3Eta"           , &lep3Eta_           ,   "lep3Eta/F");
+    tree_->Branch("lep3Id"            , &lep3Id_            ,   "lep3Id_/i");
+
+    tree_->Branch("ncosmics"        , &ncosmics_        ,   "ncosmics/i");
 
   }
 
@@ -119,6 +173,8 @@ class MitGPTreeReduced {
     tree_->SetBranchAddress("evt_weight"        , &evt_weight_ );
     tree_->SetBranchAddress("hlt_weight"        , &hlt_weight_ );
     tree_->SetBranchAddress("pu_weight"         , &pu_weight_  );
+    tree_->SetBranchAddress("pu_weightup"       , &pu_weightup_  );
+    tree_->SetBranchAddress("pu_weightdo"       , &pu_weightdo_  );
     tree_->SetBranchAddress("kf_weight"         , &kf_weight_ );
   
     tree_->SetBranchAddress("nvtx"         , &nvtx_      );
@@ -127,6 +183,7 @@ class MitGPTreeReduced {
     tree_->SetBranchAddress("sumEt"        , &sumEt_     );
     tree_->SetBranchAddress("metSig"       , &metSig_    );
   
+    tree_->SetBranchAddress("nphotons"      , &nphotons_);
     tree_->SetBranchAddress("phoEt"         , &phoEt_     );
     tree_->SetBranchAddress("phoEta"        , &phoEta_    );
     tree_->SetBranchAddress("phoPhi"        , &phoPhi_    );
@@ -134,12 +191,35 @@ class MitGPTreeReduced {
     tree_->SetBranchAddress("phoMetDeltaPhi", &phoMetDeltaPhi_ );
   
     tree_->SetBranchAddress("njets"         , &njets_    );
-    tree_->SetBranchAddress("leadJetEt"         , &leadJetEt_    );
+    tree_->SetBranchAddress("leadJetPt"         , &leadJetPt_    );
     tree_->SetBranchAddress("leadJetEta"        , &leadJetEta_   );
     tree_->SetBranchAddress("leadJetPhi"        , &leadJetPhi_   );
-    tree_->SetBranchAddress("trailJetEt"        , &trailJetEt_    );
+    tree_->SetBranchAddress("trailJetPt"        , &trailJetPt_    );
     tree_->SetBranchAddress("trailJetEta"       , &trailJetEta_   );
     tree_->SetBranchAddress("trailJetPhi"       , &trailJetPhi_   );
+
+    tree_->SetBranchAddress("nalljets"          , &nalljets_          );
+    tree_->SetBranchAddress("jet1Pt"            , &jet1Pt_            );
+    tree_->SetBranchAddress("jet1Eta"           , &jet1Eta_           );
+    tree_->SetBranchAddress("jet2Pt"            , &jet2Pt_            );
+    tree_->SetBranchAddress("jet2Eta"           , &jet2Eta_           );
+    tree_->SetBranchAddress("jet3Pt"            , &jet3Pt_            );
+    tree_->SetBranchAddress("jet3Eta"           , &jet3Eta_           );
+    tree_->SetBranchAddress("jet4Pt"            , &jet4Pt_            );
+    tree_->SetBranchAddress("jet4Eta"           , &jet4Eta_           );
+
+    tree_->SetBranchAddress("nlep"              , &nlep_            );
+    tree_->SetBranchAddress("lep1Pt"            , &lep1Pt_          );
+    tree_->SetBranchAddress("lep1Eta"           , &lep1Eta_         );
+    tree_->SetBranchAddress("lep1Id"            , &lep1Id_          );
+    tree_->SetBranchAddress("lep2Pt"            , &lep2Pt_          );
+    tree_->SetBranchAddress("lep2Eta"           , &lep2Eta_         );
+    tree_->SetBranchAddress("lep2Id"            , &lep2Id_          );
+    tree_->SetBranchAddress("lep3Pt"            , &lep3Pt_          );
+    tree_->SetBranchAddress("lep3Eta"           , &lep3Eta_         );
+    tree_->SetBranchAddress("lep3Id"            , &lep3Id_          );
+
+    tree_->SetBranchAddress("ncosmics",       &ncosmics_);
 
     gErrorIgnoreLevel = currentState;
   }
@@ -156,6 +236,8 @@ MitGPTreeReduced::InitVariables(){
   evt_weight_ = -1.;
   hlt_weight_ = -1.;
   pu_weight_  = -1.;
+  pu_weightup_ = -1.;
+  pu_weightdo_ = -1.;
   kf_weight_  = -1.;
 
   nvtx_  = 0;
@@ -164,6 +246,7 @@ MitGPTreeReduced::InitVariables(){
   sumEt_ = -1.;
   metSig_= -1.;
 
+  nphotons_ = 0;
   phoEt_ = -1.;
   phoEta_= -100.;
   phoPhi_= -100.;
@@ -171,12 +254,36 @@ MitGPTreeReduced::InitVariables(){
   phoMetDeltaPhi_ = -100.;
 
   njets_ = 0;
-  leadJetEt_ = -1.;
+  leadJetPt_ = -1.;
   leadJetEta_= -100.;
   leadJetPhi_= -100.;
-  trailJetEt_ = -1.;
+  trailJetPt_ = -1.;
   trailJetEta_= -100.;
   trailJetPhi_= -100.;
+  
+  njets_ = 0;
+  jet1Pt_ = -1.;
+  jet1Eta_ = -100.;
+  jet2Pt_ = -1.;
+  jet2Eta_ = -100.;
+  jet3Pt_ = -1.;
+  jet3Eta_ = -100.;
+  jet4Pt_ = -1.;
+  jet4Eta_ = -100.;
+
+  nlep_ = 0;
+  lep1Pt_ = -1.;
+  lep1Eta_ = -100.;
+  lep1Id_ = 0;
+  lep2Pt_ = -1.;
+  lep2Eta_ = -100.;
+  lep2Id_ = 0;
+  lep3Pt_ = -1.;
+  lep3Eta_ = -100.;
+  lep3Id_ = 0;
+
+  ncosmics_ = 0;
+
 }
 
 #endif
--- Utils/interface/TreeReducer.h
+++ Utils/interface/TreeReducer.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: TreeReducer.h,v 1.2 2013/08/23 22:47:34 dimatteo Exp $
+// $Id: TreeReducer.h,v 1.3 2013/08/26 21:59:38 dimatteo Exp $
 //
 // TreeReducer
 //
@@ -30,6 +30,8 @@ namespace mithep
 
     // Various parameters
     void                 SetPUTarget(const TH1D *h) { fPUTarget = h; }
+    void                 SetPUTargetUp(const TH1D *h) { fPUTargetUp = h; }
+    void                 SetPUTargetDown(const TH1D *h) { fPUTargetDown = h; }
     void                 SetOutput(TFile *f)  { fOutFile = f; }
     void                 SetVerbose(bool b) { fVerbose = b; }
     void                 SetInputBaseDir(const TString s)  { fInputBaseDir = s; }
@@ -39,6 +41,8 @@ namespace mithep
     // Members
     const Sample * fSample;   // sample to be reduced
     const TH1D   * fPUTarget; // target PU histo
+    const TH1D   * fPUTargetUp; // target PU histo up
+    const TH1D   * fPUTargetDown; // target PU histo down
     TFile        * fOutFile;  // target out file
 
     bool           fVerbose;      // print out what the reducer is doing
@@ -47,9 +51,13 @@ namespace mithep
 
     // Auxiliary functions
     static const TH1D   *sPUWeights;
+    static const TH1D   *sPUWeightsUp;
+    static const TH1D   *sPUWeightsDown;
     float                PUWeight(Float_t npu);  // PU reweighting function
+    float                PUWeightUp(Float_t npu);  // PU reweighting function
+    float                PUWeightDown(Float_t npu);  // PU reweighting function
     float                KFactorWeight(Float_t scale, Float_t phet);  // KFactor reweighting function
-    bool                 EventIsSelected(MitGPTree &tree, int treeType);    // Selection function
+    bool                 EventIsSelected(MitGPTree &tree, int treeType, int& theGoodPhoton);    // Selection function
     bool                 PhotonIsSelected(float R9, float HoverE, float CovIetaIeta, float Iso1, float Iso2, float Iso3);    // Photon id function
     float                GetCorrDeltaPhi(float phi1, float phi2);    // Corr delta phi calculator
 
